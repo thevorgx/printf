1d659e59da62d3260f4005701a0f6ba4202be164
thevorgx <kh.elallali@gmail.com>
2023-08-20 07:25:17 +0100

    add NULL to struct

--- _printf.c
+++ _printf.c
@@ -79,7 +79,8 @@ int check_specifier(const char f, va_list args)
 		{'s', print_string_s},
 		{'%', print_pourcen},
 		{'i', _printint},
-		{'d', _printint}
+		{'d', _printint},
+		{NULL, NULL}
 	};
 	structsize = sizeof(checker_fct) / sizeof(checker_fct[0]) - 1;
 	while (structsize >= 0)
