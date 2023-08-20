8f761ef82c6ba7488fdc2de90d3c0c6081200536
thevorgx <kh.elallali@gmail.com>
2023-08-20 07:06:56 +0100

    _putint add to _putchar file

--- _putchar.c
+++ _putchar.c
@@ -37,3 +37,30 @@ int _puts(char *s)
 	}
 	return (i);
 }
+
+int _putint(int n)
+{
+	int count = 0;
+	unsigned int num;
+	
+	if (n < 0)
+	{
+		_putchar ('-');
+		count++;
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+	{
+		count+= _putint(num / 10);
+	}
+
+	_putchar((num % 10) + '0');
+	count++;
+	
+	return (count);
+}
